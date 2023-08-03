b943f67654f7249dfd31fe933ac67add178d2b45
Ryan Meyers <rhynogolfer42@gmail.com>
2023-08-03 16:25:16 -0600

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0,2)
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (muliplyValue) {
+    return function (value) {
+        return muliplyValue * value
+    }
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers,func){
+    return func(drivers);
+}
